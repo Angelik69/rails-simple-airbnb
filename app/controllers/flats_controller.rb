95c0bd44139250264e86f81e3bb14e38179fd79a
Guillaume Bonjean <gbonjean@free.fr>
2023-02-21 12:55:49 +0100

    simple airbnb ok

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,8 @@
 class FlatsController < ApplicationController
   def index
     @flats = Flat.all
+    @searched = params[:query] && params[:query] != ''
+    @flats = Flat.where("name LIKE '%#{params[:query]}%'") if @searched
   end
 
   def new
@@ -9,11 +11,39 @@ class FlatsController < ApplicationController
 
   def create
     @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to flats_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
   end
 
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :adress, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :picture_url)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+PICS = %w[ https://images.unsplash.com/photo-1570129477492-45c003edd2be https://images.unsplash.com/photo-1568605114967-8130f3a36994 https://images.unsplash.com/photo-1600596542815-ffad4c1539a9].freeze
+
 
 5.times {
   Flat.create!(
@@ -12,6 +14,7 @@
     address: '10 Clifton Gardens London W9 1DT',
     description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
     price_per_night: 75,
-    number_of_guests: 3
+    number_of_guests: 3,
+    picture_url: PICS.sample
   )
 }
